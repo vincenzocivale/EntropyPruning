1d28ae52bd021f65a023a6bf72470ae28ed0b0ad
vincenzocivale <civalevincenzoyuto@gmail.com>
2026-06-29 15:46:20 +0200

    feat(wsi): add ABMIL checkpoint utilities

--- src/models/wsi/__init__.py
+++ src/models/wsi/__init__.py
@@ -1,6 +1,12 @@
 """WSI-level models."""
 
 from src.models.wsi.abmil import ABMILClassifier, ABMILOutput
+from src.models.wsi.abmil_checkpoint import (
+    ABMILClassifierCheckpoint,
+    ABMILClassifierConfig,
+    load_abmil_classifier_checkpoint,
+    save_abmil_classifier_checkpoint,
+)
 from src.models.wsi.checkpoint import (
     WSITileAttentionForecasterCheckpoint,
     WSITileAttentionForecasterConfig,
@@ -14,11 +20,15 @@ from src.models.wsi.tile_attention_forecaster import (
 
 __all__ = [
     "ABMILClassifier",
+    "ABMILClassifierCheckpoint",
+    "ABMILClassifierConfig",
     "ABMILOutput",
     "WSITileAttentionForecaster",
     "WSITileAttentionForecasterCheckpoint",
     "WSITileAttentionForecasterConfig",
+    "load_abmil_classifier_checkpoint",
     "load_wsi_tile_attention_forecaster_checkpoint",
+    "save_abmil_classifier_checkpoint",
     "save_wsi_tile_attention_forecaster_checkpoint",
     "wsi_attention_kl_loss",
 ]
