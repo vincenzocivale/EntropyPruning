f665504612bb5359b1d977ec4059ad6dec1cd5fe
vincenzocivale <civalevincenzoyuto@gmail.com>
2026-06-29 12:29:41 +0200

    feat(wsi): add forecaster checkpoint utilities

--- src/models/wsi/__init__.py
+++ src/models/wsi/__init__.py
@@ -1,8 +1,21 @@
 """WSI-level models."""
 
+from src.models.wsi.checkpoint import (
+    WSITileAttentionForecasterCheckpoint,
+    WSITileAttentionForecasterConfig,
+    load_wsi_tile_attention_forecaster_checkpoint,
+    save_wsi_tile_attention_forecaster_checkpoint,
+)
 from src.models.wsi.tile_attention_forecaster import (
     WSITileAttentionForecaster,
     wsi_attention_kl_loss,
 )
 
-__all__ = ["WSITileAttentionForecaster", "wsi_attention_kl_loss"]
+__all__ = [
+    "WSITileAttentionForecaster",
+    "WSITileAttentionForecasterCheckpoint",
+    "WSITileAttentionForecasterConfig",
+    "load_wsi_tile_attention_forecaster_checkpoint",
+    "save_wsi_tile_attention_forecaster_checkpoint",
+    "wsi_attention_kl_loss",
+]
